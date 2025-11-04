4cf14baf7b8e9cf904e4b9dd7586311113b66342
Ubuntu <ubuntu@ip-172-31-9-163.ap-south-1.compute.internal>
2025-11-04 12:21:13 +0000

    Trigger Jenkins via webhook

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Jenkins Python Demo running successfully!"
+    return "->Jenkins Python Demo running successfully!"
 
 def add(a, b):
     return a + b
