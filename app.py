9df49702c1bc4d37895ac9c8e114976308110884
Ubuntu <ubuntu@ip-172-31-9-163.ap-south-1.compute.internal>
2025-11-04 07:56:37 +0000

    flask add

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Jenkins Python Demo running successfully!"
+
 def add(a, b):
     return a + b
 
 if __name__ == "__main__":
     result = add(10, 20)
     print(f"The result is: {result}")
+    app.run(host="0.0.0.0", port=5000)
+
